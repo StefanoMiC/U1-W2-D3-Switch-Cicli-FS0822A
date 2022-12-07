b606f3067f53b8cbc24a9eb74c8fa089b4f03149
Stefano Miceli <mic@nucleode.com>
2022-12-07 13:23:35 +0100

    adds conditionals

---
+++ conditionals.js
@@ -0,0 +1,17 @@
+let condition = true;
+
+if (condition) {
+  //     // ENTRO QUI SOLO SE contition È true
+  // if(){
+  //     if(){
+  //     } else {
+  //         if(){
+  //         } else {
+  //             if(){
+  //             } else {
+  //             }
+  //         }
+  //     }
+  // } else {
+  // }
+}
